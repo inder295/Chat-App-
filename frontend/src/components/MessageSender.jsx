c8111ee871a0e2994b20d5d2c5fea21354bf5312
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2026-03-01 22:45:05 +0530

    added some stuff

--- frontend/src/components/MessageSender.jsx
+++ frontend/src/components/MessageSender.jsx
@@ -1,33 +1,56 @@
-import { GalleryHorizontal } from 'lucide-react';
+import { GalleryHorizontal ,SendHorizontal} from 'lucide-react';
 import { useState } from 'react';
+import { useMessage } from '../store/useMessages';
+
 
 export const MessageSender = () => {
 
-  const formData=useState({
+  const [formData,setFormData]=useState({
     text:"",
-    image:""
+    image:null
   });
 
-  function handleSubmit(e){
-    e.target.preventDefault();
-    
-    
+  const {sentMessage,sendingMessage}=useMessage();
+  const selectedUser=useMessage((state)=>state.selectedUser)
+
+  async function handleSubmit(e){
+    e.preventDefault();      
+    await sentMessage(selectedUser._id,formData);
+    setFormData({text:"",image:null});
   }
+
+  function handleChange(e){
+      const {name,value,files}=e.target;
+
+      if(name=="image"){
+        setFormData(prev=> ({...prev,image:files[0]}))
+      }else{
+        setFormData(prev=> ({...prev,[name]:value }))
+      }
+  }
+
+  
   
 
 
   return (
-    <form className='flex ' onClick={handleSubmit}>
+    <form className='flex ' onSubmit={handleSubmit}>
       
-          <input type="text" className='w-full focus-within:outline-neutral-500 p-2.5 border-2 border-gray-200 rounded m-2  '  placeholder='Add message'  
+          <input type="text" className='w-full focus-within:outline-neutral-500 p-2.5 border-2 border-gray-200 rounded-2xl m-2 '  placeholder='Add message' name="text" value={formData.text} onChange={handleChange}
            />
             
 
-          <div className='hover:bg-gray-100 cursor-pointer rounded-full mx-2'>
-              <input type='file' className='hidden' id="fileUpload" />
+          <div className='hover:bg-gray-100 cursor-pointer rounded-full ml-2'>
+              <input type='file' className='hidden' id="fileUpload" name="image" onChange={handleChange} />
               <label htmlFor="fileUpload">
-                <GalleryHorizontal className='w-8 h-6 m-2  my-5 mx-4 text-gray-600  cursor-pointer' />
+                <GalleryHorizontal className='w-8 h-6  my-5 ml-4  text-gray-600  cursor-pointer' />
               </label>
+          </div>
+
+          <div className='hover:bg-gray-100 cursor-pointer rounded-full mx-2'>
+              <button type="submit" disabled={sendingMessage}>
+                <SendHorizontal className='w-8 h-6 mr-2  my-5 text-gray-600  cursor-pointer'/>
+              </button>
           </div>
 
     
