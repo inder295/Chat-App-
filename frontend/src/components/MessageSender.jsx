dad61e41139d571e6354097ca35724be7fa96ce6
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2026-03-01 19:37:41 +0530

    added some more stuff

--- frontend/src/components/MessageSender.jsx
+++ frontend/src/components/MessageSender.jsx
@@ -1,23 +1,37 @@
 import { GalleryHorizontal } from 'lucide-react';
+import { useState } from 'react';
 
 export const MessageSender = () => {
 
+  const formData=useState({
+    text:"",
+    image:""
+  });
+
+  function handleSubmit(e){
+    e.target.preventDefault();
+    
+    
+  }
   
+
+
   return (
-    <div className='flex '>
+    <form className='flex ' onClick={handleSubmit}>
       
-          <input type="text" className='w-full focus-within:outline-neutral-500 p-2.5 border-2 border-gray-200 rounded m-2  ' placeholder='Add message' />
+          <input type="text" className='w-full focus-within:outline-neutral-500 p-2.5 border-2 border-gray-200 rounded m-2  '  placeholder='Add message'  
+           />
             
 
-       <div className='hover:bg-gray-100 cursor-pointer rounded-full mx-2'>
-          <input type='file' className='hidden' id="fileUpload" />
-          <label htmlFor="fileUpload">
-            <GalleryHorizontal className='w-8 h-6 m-2  my-5 mx-4 text-gray-600  cursor-pointer' />
-          </label>
+          <div className='hover:bg-gray-100 cursor-pointer rounded-full mx-2'>
+              <input type='file' className='hidden' id="fileUpload" />
+              <label htmlFor="fileUpload">
+                <GalleryHorizontal className='w-8 h-6 m-2  my-5 mx-4 text-gray-600  cursor-pointer' />
+              </label>
+          </div>
 
-       </div>
     
-    </div>
+    </form>
   )
 }
 
--- frontend/src/store/useMessages.js
+++ frontend/src/store/useMessages.js
@@ -2,12 +2,13 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import { toast } from "react-toastify";
 
-export const useMessage=create((set)=>({
+export const useMessage=create((set,get)=>({
     fetchingUsers:false,
     selectedUser:null,
     users:[],
     messages:[],
     fetchingMessages:false,
+    sendingMessage:false,
 
     setSelectedUser:((user)=>set({selectedUser:user})),
 
@@ -39,6 +40,21 @@ export const useMessage=create((set)=>({
           set({fetchingMessages:false})  
            
         }
+    },
+    sentMessage:async(id,formData)=>{
+        try {
+           set({sendingMessage:true})
+           const res=await axiosInstance.post(`/message/send/${id}`,formData);
+           set({messages:[...get().messages,res.data]}) 
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error);
+            
+        }finally{
+           set({sendingMessage:false}) 
+           
+        }
+
     }
 
 }))
